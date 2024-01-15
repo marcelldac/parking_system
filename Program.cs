5ef6314c79cc9579b969ac80b81ead21ce1e58a0
marcelldac <dacmarcell@gmail.com>
2024-01-15 19:34:05 -0300

    build: dotnet new program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingSystem 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
